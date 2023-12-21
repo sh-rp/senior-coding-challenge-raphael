22562597b14dd8880e19568a890facdc1ef89ba1
Dave <shrps@posteo.net>
2023-12-22 00:29:49 +0100

    remove solution

--- src/model_generator.py
+++ src/model_generator.py
@@ -8,36 +8,8 @@ from pydantic import BaseModel, create_model
 
 
 def _generate_model(name: str, schema: TTableSchema) -> Type[BaseModel]:
-    # implement me
-
-    # START: THIS WILL BE REMOVED FOR THE HOMEWORK
-    type_map: Dict[TDataType, Any] = {
-        "text": str,
-        "double": float,
-        "bool": bool,
-        "timestamp": int,
-        "bigint": int,
-        "binary": bytes,
-        "complex": Dict[Any, Any],
-        "decimal": Decimal,
-        "wei": int,
-        "date": date,
-        "time": datetime,
-    }
-
-    fields: Dict[str, Any] = {}
-    for cname, column in schema["columns"].items():
-        if cname == "schema":
-            continue
-        fields[cname] = (
-            (type_map[column["data_type"]] | None, None)
-            if column.get("nullable", False) is True
-            else (type_map[column["data_type"]], ...)
-        )
-        print(fields)
-
-    return create_model(name + "_model", **fields)  # type: ignore
-    # END: THIS WILL BE REMOVED FOR THE HOMEWORK
+    # TODO: IMPLEMENT ME
+    return BaseModel
 
 
 def generate_models(schema: Schema) -> Dict[str, Type[BaseModel]]:
