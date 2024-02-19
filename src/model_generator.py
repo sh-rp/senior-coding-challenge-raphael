3a3f83b057524b8abd38104599f369a0e2ec47a0
Raphael Lullis <raphael@mushroomlabs.com>
2024-02-19 16:05:11 +0100

    Implementation of model_generator

--- src/model_generator.py
+++ src/model_generator.py
@@ -1,15 +1,46 @@
-from typing import Dict, Type, Optional, Any
-from datetime import date, time, datetime
+from datetime import datetime
 from decimal import Decimal
+from typing import Dict, Optional, Type
 
 from dlt.common.schema import Schema, TTableSchema
-from dlt.common.schema.typing import TDataType
 from pydantic import BaseModel, create_model
+from pydantic.fields import FieldInfo
 
 
 def _generate_model(name: str, schema: TTableSchema) -> Type[BaseModel]:
-    # TODO: IMPLEMENT ME
-    return BaseModel
+    columns = schema["columns"]
+    # If the name of the column in the schema might shadow an
+    # attribute for the base model, we get an error. So we need to
+    # check names that we should use an alias
+    protected_names = ["schema"]
+    column_type_map = {
+        "bool": bool,
+        "timestamp": datetime,
+        "bigint": int,
+        "text": str,
+        "decimal": Decimal,
+        "complex": Dict,
+    }
+
+    fields = {}
+
+    for column, column_spec in columns.items():
+        required = not column_spec["nullable"]
+
+        field_name = column if column not in protected_names else f"_{column}"
+        data_type = column_type_map.get(str(column_spec["data_type"]))
+
+        assert data_type is not None
+
+        # I'd rather find a more proper way to do this check, but
+        # apparently create_model is not meant to be used for more
+        # complex field definitions
+        if not required:
+            data_type = Optional[data_type]  # type: ignore
+
+        fields[field_name] = (data_type, FieldInfo(alias=column))
+
+    return create_model(name, **fields)  # type: ignore
 
 
 def generate_models(schema: Schema) -> Dict[str, Type[BaseModel]]:
